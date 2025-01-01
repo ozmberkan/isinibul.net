b1804031b7bc9a854802d0149f8b00076936acb8
ozmberkan <ozmberkan@gmail.com>
2025-01-01 15:24:35 +0300

    Hatalar çözümlendi.

--- app/(root)/ilanlar/page.tsx
+++ app/(root)/ilanlar/page.tsx
@@ -1,11 +1,21 @@
 "use client";
-import { useSearchParams } from "next/navigation";
 
-const Ilanlar = () => {
-  const searchParams = useSearchParams();
-  const q = searchParams.get("q");
+import { useSearchParams } from "next/navigation";
+import React, { Suspense } from "react";
+
+const IlanlarContent = () => {
+  const params = useSearchParams();
+  const q = params.get("q");
 
   return <div>{q}</div>;
+};
+
+const Ilanlar = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <IlanlarContent />
+    </Suspense>
+  );
 };
 
 export default Ilanlar;
